4b55f48ace0db5e70e1d6e4b93033dcd4da81631
Sebastian Stark <sstark@mailbox.org>
2024-06-22 01:42:21 +0200

    simplify

--- traycortex.py
+++ traycortex.py
@@ -13,7 +13,11 @@ image_i_r = Image.open("borgmatic_i_r.png")
 def after_click(icon, query):
     global run_checker
     global run_runner
-    if str(query) == "Engage":
+    if str(query) == "Icon Black":
+        icon.icon = image
+    elif str(query) == "Icon White":
+        icon.icon = image_i
+    elif str(query) == "Engage":
         runq.put("True")
     elif str(query) == "Discard":
         run_checker = False
@@ -25,34 +29,28 @@ def notif(icon, _):
     icon.notify("bla")
 
 
-def ch_icon(icon, query):
-    if str(query) == "Icon Black":
-        icon.icon = image
-    if str(query) == "Icon White":
-        icon.icon = image_i
-
-
 def check_borgmatic():
     while run_checker:
         time.sleep(1)
 
+
 def run_borgmatic():
     while run_runner:
         runq.get()
+        icon.notify("Starting backup")
         icon.icon = image_i_r
-        icon.title = "Running"
         print("Running borgmatic...")
         time.sleep(5)
         print("Done.")
         icon.icon = image_i
-        icon.title = "Idle"
+        icon.notify("Finished backup")
 
 
 icon = pystray.Icon("bmsystray", image_i, "Borgmatic", menu=pystray.Menu(
     pystray.MenuItem("Engage", after_click),
     pystray.MenuItem("Notify", notif),
-    pystray.MenuItem("Icon Black", ch_icon),
-    pystray.MenuItem("Icon White", ch_icon),
+    pystray.MenuItem("Icon Black", after_click),
+    pystray.MenuItem("Icon White", after_click),
     pystray.MenuItem("Discard", after_click)))
 
 checker = threading.Thread(target=check_borgmatic)
