57f103c651e72620d6da7c9056917e3ad7fe22d3
Sebastian Stark <sstark@mailbox.org>
2024-06-21 23:26:19 +0200

    better test menu

--- bmsystray.py
+++ bmsystray.py
@@ -1,28 +1,17 @@
 import pystray
 from PIL import Image
 
-image = Image.open("borgmatic.png")
+image = Image.open("borgmatic_i.png")
 
 def after_click(icon, query):
-    if str(query) == "GeeksforGeeks Website":
-        print("The Best Place to learn anything Tech \
-        Related -> https://www.geeksforgeeks.org/")
-        # icon.stop()
-    elif str(query) == "GeeksforGeeks Youtube":
-        print("Youtube Channel of GeeksforGeeks \
-        is -> https://www.youtube.com/@GeeksforGeeksVideos")
-        # icon.stop()
-    elif str(query) == "GeeksforGeeks LinkedIn":
-        print("LinkedIn of GeeksforGeeks \
-        is -> https://www.linkedin.com/company/geeksforgeeks/")
+    if str(query) == "Run Borgmatic":
+        print("Running borgmatic...")
     elif str(query) == "Exit":
         icon.stop()
 
 
-icon = pystray.Icon("GFG", image, "GeeksforGeeks", menu=pystray.Menu(
-    pystray.MenuItem("GeeksforGeeks Website", after_click),
-    pystray.MenuItem("GeeksforGeeks Youtube", after_click),
-    pystray.MenuItem("GeeksforGeeks LinkedIn", after_click),
+icon = pystray.Icon("bmsystray", image, "Borgmatic", menu=pystray.Menu(
+    pystray.MenuItem("Run Borgmatic", after_click),
     pystray.MenuItem("Exit", after_click)))
  
 icon.run()
