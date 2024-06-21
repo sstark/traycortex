9d55b7e69c79024d450985270c955ce576603595
Sebastian Stark <sstark@mailbox.org>
2024-06-22 00:27:35 +0200

    checker thread

--- bmsystray.py
+++ bmsystray.py
@@ -1,17 +1,22 @@
 import pystray
 from PIL import Image
+import threading
+import time
 
+run_checker = True
 image = Image.open("borgmatic.png")
 image_i = Image.open("borgmatic_i.png")
 
 def after_click(icon, query):
+    global run_checker
     if str(query) == "Run Borgmatic":
         print("Running borgmatic...")
     elif str(query) == "Exit":
+        run_checker = False
         icon.stop()
 
 
-def notif(icon, query):
+def notif(icon, _):
     icon.notify("bla")
 
 
@@ -22,11 +27,23 @@ def ch_icon(icon, query):
         icon.icon = image_i
 
 
+def check_borgmatic():
+    while run_checker:
+        time.sleep(1)
+        if icon.icon == image:
+            icon.icon = image_i
+            continue
+        if icon.icon == image_i:
+            icon.icon = image
+
+
 icon = pystray.Icon("bmsystray", image_i, "Borgmatic", menu=pystray.Menu(
     pystray.MenuItem("Run Borgmatic", after_click),
     pystray.MenuItem("Notify", notif),
     pystray.MenuItem("Icon Black", ch_icon),
     pystray.MenuItem("Icon White", ch_icon),
     pystray.MenuItem("Exit", after_click)))
- 
+
+checker = threading.Thread(target=check_borgmatic)
+checker.start()
 icon.run()
