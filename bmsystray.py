807bd7e8d99b8308e50ee0792da5bfe7fd74365e
Sebastian Stark <sstark@mailbox.org>
2024-06-21 23:53:05 +0200

    changeable icon

--- bmsystray.py
+++ bmsystray.py
@@ -1,7 +1,8 @@
 import pystray
 from PIL import Image
 
-image = Image.open("borgmatic_i.png")
+image = Image.open("borgmatic.png")
+image_i = Image.open("borgmatic_i.png")
 
 def after_click(icon, query):
     if str(query) == "Run Borgmatic":
@@ -10,9 +11,22 @@ def after_click(icon, query):
         icon.stop()
 
 
-icon = pystray.Icon("bmsystray", image, "Borgmatic", menu=pystray.Menu(
+def notif(icon, query):
+    icon.notify("bla")
+
+
+def ch_icon(icon, query):
+    if str(query) == "Icon Black":
+        icon.icon = image
+    if str(query) == "Icon White":
+        icon.icon = image_i
+
+
+icon = pystray.Icon("bmsystray", image_i, "Borgmatic", menu=pystray.Menu(
     pystray.MenuItem("Run Borgmatic", after_click),
+    pystray.MenuItem("Notify", notif),
+    pystray.MenuItem("Icon Black", ch_icon),
+    pystray.MenuItem("Icon White", ch_icon),
     pystray.MenuItem("Exit", after_click)))
  
 icon.run()
-
