3c56438aa0a4942a9f397787c1aa61158f3f56a6
Sebastian Stark <sstark@mailbox.org>
2024-06-23 01:26:02 +0200

    structure ok but threading broken

--- traycortex/tray.py
+++ traycortex/tray.py
@@ -4,7 +4,8 @@ from PIL import Image
 import threading
 import time
 import queue
-
+from importlib import resources
+from traycortex import images
 
 p_name = __package__ or __name__
 title = "Borgmatic"
@@ -12,12 +13,13 @@ run_checker = True
 run_runner = True
 darkmode = True
 
+res = resources.files(images)
 # standard image and image while running
-image = Image.open("borgmatic.png")
-image_r = Image.open("borgmatic.png")
+image = Image.open(res / "borgmatic.png")
+image_r = Image.open(res / "borgmatic.png")
 # same for darkmode (inverted)
-image_i = Image.open("borgmatic_i.png")
-image_i_r = Image.open("borgmatic_i_r.png")
+image_i = Image.open(res / "borgmatic_i.png")
+image_i_r = Image.open(res / "borgmatic_i_r.png")
 
 
 def get_image(running: bool = False, darkmode: bool = darkmode) -> Image.Image:
@@ -25,26 +27,28 @@ def get_image(running: bool = False, darkmode: bool = darkmode) -> Image.Image:
         return image_i_r if darkmode else image_r
     else:
         return image_i if darkmode else image
-        
 
 
-def create_menu() -> pystray.Menu:
+def create_menu(runq: queue.Queue) -> pystray.Menu:
     return pystray.Menu(
-        pystray.MenuItem("Engage", menu_click),
-        pystray.MenuItem("Discard", menu_click)
+        pystray.MenuItem("Engage", menu_click(runq)),
+        pystray.MenuItem("Discard", menu_click(runq))
     )
 
 
-def menu_click(icon, query):
-    global run_checker
-    global run_runner
-    if str(query) == "Engage":
-        runq.put(True)
-    elif str(query) == "Discard":
-        run_checker = False
-        run_runner = False
-        runq.put(False)
-        icon.stop()
+def menu_click(runq: queue.Queue) -> Callable:
+
+    def _menu_click(icon: pystray.Icon, query: str):
+        global run_checker
+        global run_runner
+        if query == "Engage":
+            runq.put(True)
+        elif query == "Discard":
+            run_checker = False
+            run_runner = False
+            runq.put(False)
+            icon.stop()
+    return _menu_click
 
 
 def borgmatic_checker():
@@ -68,10 +72,10 @@ def borgmatic_runner(icon: pystray.Icon, runq: queue.Queue) -> Callable:
 
 
 def app():
-    icon = pystray.Icon(p_name, get_image(), title, menu=create_menu())
+    runq = queue.Queue()
+    icon = pystray.Icon(p_name, get_image(), title, menu=create_menu(runq))
     checker = threading.Thread(target=borgmatic_checker)
     checker.start()
-    runq = queue.Queue()
     runner = threading.Thread(target=borgmatic_runner(icon, runq))
     runner.start()
     icon.run()
