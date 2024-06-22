17e9f045448d80aa80e5c8140011bedffb3837e0
Sebastian Stark <sstark@mailbox.org>
2024-06-23 00:32:30 +0200

    basic structure done

--- traycortex.py
+++ traycortex.py
@@ -4,11 +4,28 @@ import threading
 import time
 import queue
 
+
+p_name = __package__ or __name__
+title = "Borgmatic"
 run_checker = True
 run_runner = True
+darkmode = True
+
+# standard image and image while running
 image = Image.open("borgmatic.png")
+image_r = Image.open("borgmatic.png")
+# same for darkmode (inverted)
 image_i = Image.open("borgmatic_i.png")
 image_i_r = Image.open("borgmatic_i_r.png")
+
+
+def get_image(running: bool = False, darkmode: bool = darkmode) -> Image.Image:
+    if running:
+        return image_i_r if darkmode else image_r
+    else:
+        return image_i if darkmode else image
+        
+
 
 def create_menu() -> Menu:
     return Menu(
@@ -37,16 +54,16 @@ def borgmatic_checker():
 def borgmatic_runner():
     while run_runner:
         if runq.get():
-            icon.icon = image_i_r
+            icon.icon = get_image(running=True)
             icon.notify("Commencing backup...")
             print("Running borgmatic...")
             time.sleep(5)
             print("Done.")
-            icon.icon = image_i
+            icon.icon = get_image()
             icon.notify("Finished backup")
 
 
-icon = Icon("traycortex", image_i, "Borgmatic", menu=create_menu())
+icon = Icon(p_name, get_image(), title, menu=create_menu())
 
 checker = threading.Thread(target=borgmatic_checker)
 checker.start()
